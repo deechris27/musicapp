53c40fb957031393cbdfa45dcac7c05618094e57
Deepak <dd43028@gmail.com>
2020-05-08 03:19:47 +0530

    base

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import {HashRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter basename="/build">
+    <HashRouter basename="/">
      <App />
     </HashRouter>
   </React.StrictMode>,
