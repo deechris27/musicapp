139de64077ae73d9a998c191e172b69a537ca7ff
Deepak <dd43028@gmail.com>
2020-05-07 23:48:02 +0530

    design and svgs added

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
 import {Switch, Route} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import SearchPage from './pages/SearchPage';
 import LibraryPage from './pages/LibraryPage';
+import LeftNav from './components/LeftNav.Component';
 
 function App() {
   return (
-    <div className="App">
-        <Switch>
-          <Route path="/" exact component={HomePage}/>
-          <Route path="/search" component={SearchPage} />
-          <Route path="/library" component={LibraryPage} />
-        </Switch>
+    <React.Fragment>
+    <div className="outerwrap">
+      <div className="App">
+        <LeftNav />
+        <div className="main">
+          <div className="upperNav">Deepak's Music App</div>
+          <div className="mainContent">
+            <Switch>
+              <Route path="/" exact component={HomePage}/>
+              <Route path="/search" component={SearchPage} />
+              <Route path="/library" component={LibraryPage} />
+            </Switch>
+          </div>
+        </div>
+       </div>
+       <div className="musicControls">Music Player</div>
     </div>
+    </React.Fragment>
   );
 }
 
