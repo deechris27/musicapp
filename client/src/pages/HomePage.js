0a3c9910b6b4e32d7419409a755140b0cad08d8f
Deepak <dd43028@gmail.com>
2020-05-08 16:05:22 +0530

    dummy data for playlist

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -3,7 +3,6 @@ import axios from 'axios';
 import Playlists from '../components/PlaylistComponent';
 
 function HomePage() {
-    const [limit, setLimit] = useState(0);
 
     const [library, setLibrary] = useState([]);
 
@@ -16,10 +15,48 @@ function HomePage() {
         console.log("Error ")
     })
 
+    const libData = [
+        {
+            "album": "The All-American Rejects", 
+            "duration": 228, 
+            "title": "My Paper Heart", 
+            "id": 0, 
+            "artist": "The All-American Rejects"
+        }, 
+        {
+            "album": "The All-American Rejects", 
+            "duration": 260, 
+            "title": "Your Star", 
+            "id": 1, 
+            "artist": "The All-American Rejects"
+        }, 
+        {
+            "album": "The All-American Rejects", 
+            "duration": 233, 
+            "title": "Swing, Swing", 
+            "id": 2, 
+            "artist": "The All-American Rejects"
+        }, 
+        {
+            "album": "The All-American Rejects", 
+            "duration": 210, 
+            "title": "Time Stands Still", 
+            "id": 3, 
+            "artist": "The All-American Rejects"
+        }, 
+        {
+            "album": "The All-American Rejects", 
+            "duration": 183, 
+            "title": "One More Sad Song", 
+            "id": 4, 
+            "artist": "The All-American Rejects"
+        }
+    ]
+
     return (
         <React.Fragment>
             <div className="mainInner">
-                {library.map((library) => {
+                {libData.map((library) => {
                     return <div className="cardsWrap" key={library.id}>
                         <h2>{library.album}</h2>
                         <p className="subText">{"Select your album..."}</p>
